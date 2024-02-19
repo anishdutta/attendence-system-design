c296730a264ef9064d60bf033841e84a1ff4d372
Anish Dutta <anish2000.ad@gmail.com>
2024-02-20 01:02:02 +0530

    chore: added transaction

--- src/modules/attendance/attendance.service.ts
+++ src/modules/attendance/attendance.service.ts
@@ -1,3 +1,4 @@
+import { Transaction } from "sequelize";
 import {
   ICheckInOrOutRequest,
   ICheckInOutType,
@@ -12,79 +13,12 @@ export class AttendenceService {
 
   constructor() {
     this.sessions = new Sessions();
-  }
+  };
 
-  async checkInAndOutInstructor(
-    request: ICheckInOrOutRequest
-  ): Promise<ISession | ISession[]> {
-    switch (request.type) {
-      case ICheckInOutType.CHECK_IN:
-        return await this.handleCheckInRequest(request);
-      case ICheckInOutType.CHECK_OUT:
-        return await this.handleCheckOutRequest(request);
-      default:
-        throw new Error("There are no open sessions at this moment");
-    }
-  }
-
-  private async handleCheckInRequest(
-    request: ICheckInOrOutRequest
-  ): Promise<ISession> {
-    const todayAt0Hours = new Date();
-    todayAt0Hours.setHours(0);
-    const openSessions = await this.sessions.getSessionsByInstructorId(
-      request.instructorId,
-      ISessionStatus.OPEN,
-      todayAt0Hours
-    );
-    console.log(openSessions);
-    if (openSessions && openSessions.length > 0) {
-      throw new Error("There is already an open session at this moment");
-    }
-    const session = await this.sessions.createSession({
-      checkInTime: new Date(),
-      instructorId: request.instructorId,
-      status: ISessionStatus.OPEN,
-    });
-    return session;
-  }
-
-  private async handleCheckOutRequest(
-    request: ICheckInOrOutRequest
-  ): Promise<ISession[]> {
-    const todayAt0Hours = new Date();
-    todayAt0Hours.setHours(0);
-    const openSessions = await this.sessions.getSessionsByInstructorId(
-      request.instructorId,
-      ISessionStatus.OPEN,
-      todayAt0Hours
-    );
-    if (!openSessions || openSessions.length === 0) {
-      throw new Error("There are no open sessions at this moment");
-    }
-    const checkOutTime = new Date();
-    await this.sessions.updateSession({
-      ids: openSessions.map((session) => session.id),
-      status: ISessionStatus.CLOSED,
-      checkOutTime: checkOutTime,
-    });
-    return this.constructCheckOutSessions(openSessions.map(session=>session['dataValues']),checkOutTime);
-  }
-
-  private constructCheckOutSessions(sessions:ISession[],checkOutTime:Date):ISession[]{
-    return sessions.map(session=>{
-        const finalSession= {
-            ...session,
-            status: ISessionStatus.CLOSED,
-            checkOutTime: checkOutTime,
-        }
-        return {
-            ...finalSession,
-            totalWorkingHours: calculateTotalWorkingHours(new Date(finalSession.checkInTime).valueOf(),new Date(finalSession.checkOutTime).valueOf())
-        } as ISession
-    });
-  }
-
+  /**
+   * Retrieves monthly analytics data for instructors.
+   * @returns A promise that resolves to an array of objects containing instructor IDs and total working hours.
+   */
   async getMonthlyAnalytics(): Promise<IGetMonthlyAnalytics[]> {
     const monthBackDate = new Date();
     monthBackDate.setMonth(monthBackDate.getMonth() - 1);
@@ -106,4 +40,96 @@ export class AttendenceService {
         } as IGetMonthlyAnalytics
     });
   }
+
+  /**
+   * Checks in or checks out an instructor based on the request type.
+   * @param {ICheckInOrOutRequest} request - The request object containing the type of check-in or check-out.
+   * @returns {Promise<ISession | ISession[]>} - A promise that resolves to the checked-in or checked-out session(s).
+   * @throws {Error} - If there are no open sessions at the moment.
+   */
+  async checkInAndOutInstructor(
+    request: ICheckInOrOutRequest
+  ): Promise<ISession | ISession[]> {
+    const t = await this.sessions.getTransactions();
+    let response:ISession | ISession[];
+    switch (request.type) {
+      case ICheckInOutType.CHECK_IN:
+        response =  await this.handleCheckInRequest(request,t);
+        break;
+      case ICheckInOutType.CHECK_OUT:
+        response =  await this.handleCheckOutRequest(request,t);
+        break;
+      default:
+        throw new Error("There are no open sessions at this moment");
+    }
+    await this.sessions.transaction.commit();
+    return response;
+  };
+
+
+   /**
+   * -----  Private Methods --------
+   */
+
+  private async handleCheckInRequest(
+    request: ICheckInOrOutRequest,
+    transaction: Transaction
+  ): Promise<ISession> {
+    const todayAt0Hours = new Date();
+    todayAt0Hours.setHours(0);
+    const openSessions = await this.sessions.getSessionsByInstructorId(
+      request.instructorId,
+      ISessionStatus.OPEN,
+      todayAt0Hours,
+      transaction
+    );
+    console.log(openSessions);
+    if (openSessions && openSessions.length > 0) {
+      throw new Error("There is already an open session at this moment");
+    }
+    const session = await this.sessions.createSession({
+      checkInTime: new Date(),
+      instructorId: request.instructorId,
+      status: ISessionStatus.OPEN,
+    },transaction);
+    return session;
+  };
+
+  private async handleCheckOutRequest(
+    request: ICheckInOrOutRequest,
+    transaction: Transaction
+  ): Promise<ISession[]> {
+    const todayAt0Hours = new Date();
+    todayAt0Hours.setHours(0);
+    const openSessions = await this.sessions.getSessionsByInstructorId(
+      request.instructorId,
+      ISessionStatus.OPEN,
+      todayAt0Hours,
+      transaction
+    );
+    if (!openSessions || openSessions.length === 0) {
+      throw new Error("There are no open sessions at this moment");
+    }
+    const checkOutTime = new Date();
+    await this.sessions.updateSession({
+      ids: openSessions.map((session) => session.id),
+      status: ISessionStatus.CLOSED,
+      checkOutTime: checkOutTime,
+    },transaction);
+    return this.constructCheckOutSessions(openSessions.map(session=>session['dataValues']),checkOutTime);
+  };
+
+  private constructCheckOutSessions(sessions:ISession[],checkOutTime:Date):ISession[]{
+    return sessions.map(session=>{
+        const finalSession= {
+            ...session,
+            status: ISessionStatus.CLOSED,
+            checkOutTime: checkOutTime,
+        }
+        return {
+            ...finalSession,
+            totalWorkingHours: calculateTotalWorkingHours(new Date(finalSession.checkInTime).valueOf(),new Date(finalSession.checkOutTime).valueOf())
+        } as ISession
+    });
+  };
 }
--- src/modules/attendance/sessions.model.ts
+++ src/modules/attendance/sessions.model.ts
@@ -1,110 +1,158 @@
-import { CreationOptional, DataTypes, Model, where } from "sequelize";
-import { ICreateSessionsRequest, ISession, ISessionStatus, IUpdateSessionRequest } from "./attendance.interface";
+import {
+  CreationOptional,
+  DataTypes,
+  Model,
+  Transaction,
+  where,
+} from "sequelize";
+import {
+  ICreateSessionsRequest,
+  ISession,
+  ISessionStatus,
+  IUpdateSessionRequest,
+} from "./attendance.interface";
 import { sequelize } from "../../config/sequelize.config";
 import { Op } from "sequelize";
 
- const bulkUpdateMethod = (session) => {
-    if (session.changed('checkOutTime') || session.changed('checkInTime')) {
-      const checkInTime = new Date(session.getDataValue('checkInTime')).valueOf();
-      const checkOutTime = new Date(session.getDataValue('checkOutTime')).valueOf();
-      const totalWorkingHours = calculateTotalWorkingHours(checkInTime,checkOutTime);
-      session.setDataValue('totalWorkingHours', totalWorkingHours);
-    }
+/**
+ * Updates the total working hours in the session if the check-in time or check-out time has changed.
+ * @param {Object} session - The session object to update.
+ * @returns None
+ */
+const bulkUpdateMethod = (session) => {
+  if (session.changed("checkOutTime") || session.changed("checkInTime")) {
+    const checkInTime = new Date(session.getDataValue("checkInTime")).valueOf();
+    const checkOutTime = new Date(
+      session.getDataValue("checkOutTime")
+    ).valueOf();
+    const totalWorkingHours = calculateTotalWorkingHours(
+      checkInTime,
+      checkOutTime
+    );
+    session.setDataValue("totalWorkingHours", totalWorkingHours);
   }
+};
 
-  export const calculateTotalWorkingHours = (checkInTime:number,checkOutTime:number):number=>{
-    return Number(((checkOutTime - checkInTime) / (1000 * 60)).toFixed(2)); // Calculating minutes
-  }
+/**
+ * Calculates the total working hours based on the check-in and check-out times provided.
+ * @param {number} checkInTime - The timestamp of the check-in time.
+ * @param {number} checkOutTime - The timestamp of the check-out time.
+ * @returns {number} The total working hours as a decimal number with two decimal places.
+ */
+export const calculateTotalWorkingHours = (
+  checkInTime: number,
+  checkOutTime: number
+): number => {
+  return Number(((checkOutTime - checkInTime) / (1000 * 60)).toFixed(2)); // Calculating minutes
+};
 
 class SessionsDb extends Model<ISession> {
-    declare id: CreationOptional<number>;
-    declare createdAt: CreationOptional<Date>;
-    declare instructorId: number;
-    declare totalWorkingHours: number;
-    declare checkInTime: Date;
-    declare checkOutTime: Date;
-    declare status: ISessionStatus;
-  }
+  declare id: CreationOptional<number>;
+  declare createdAt: CreationOptional<Date>;
+  declare instructorId: number;
+  declare totalWorkingHours: number;
+  declare checkInTime: Date;
+  declare checkOutTime: Date;
+  declare status: ISessionStatus;
+}
 
 SessionsDb.init(
-    {
-        id: {
-            type: DataTypes.INTEGER.UNSIGNED,
-            autoIncrement: true,
-            primaryKey: true,
-        },
-        createdAt: {
-            type: DataTypes.DATE,
-        },
-        instructorId: {
-            type: DataTypes.INTEGER,
-        },
-        totalWorkingHours: {
-            type: DataTypes.INTEGER,
-        },
-        checkInTime: {
-            type: DataTypes.DATE,
-        },
-        checkOutTime: {
-            type: DataTypes.DATE,
-        },
-        status: {
-            type: DataTypes.STRING,
-        }
-    },{
+  {
+    id: {
+      type: DataTypes.INTEGER.UNSIGNED,
+      autoIncrement: true,
+      primaryKey: true,
+    },
+    createdAt: {
+      type: DataTypes.DATE,
+    },
+    instructorId: {
+      type: DataTypes.INTEGER,
+    },
+    totalWorkingHours: {
+      type: DataTypes.INTEGER,
+    },
+    checkInTime: {
+      type: DataTypes.DATE,
+    },
+    checkOutTime: {
+      type: DataTypes.DATE,
+    },
+    status: {
+      type: DataTypes.STRING,
+    },
+  },
+  {
     sequelize,
     tableName: "sessions",
     underscored: true,
     hooks: {
-        beforeUpdate: bulkUpdateMethod
-      }
-  });
+      beforeUpdate: bulkUpdateMethod,
+    },
+  }
+);
 
+export class Sessions {
+  transaction: Transaction;
 
+  async createSession(
+    createSessionsRequest: ICreateSessionsRequest
+  ): Promise<ISession> {
+    return await SessionsDb.create(createSessionsRequest, {
+      transaction: this.transaction,
+    });
+  }
 
-export class Sessions{
-
-    async createSession(createSessionsRequest: ICreateSessionsRequest):Promise<ISession>{
-        return await SessionsDb.create(createSessionsRequest);
-    };
-
-    async updateSession(updateSessionRequest: IUpdateSessionRequest):Promise<any>{
-        const {ids, ...rest} = updateSessionRequest;
-        return await SessionsDb.update({
-                ...rest
-            },{
-            where:{
-                id: ids.length==1 ? ids[0] : {
-                    [Op.in]:ids
+  async updateSession(
+    updateSessionRequest: IUpdateSessionRequest
+  ): Promise<any> {
+    const { ids, ...rest } = updateSessionRequest;
+    return await SessionsDb.update(
+      {
+        ...rest,
+      },
+      {
+        where: {
+          id:
+            ids.length == 1
+              ? ids[0]
+              : {
+                  [Op.in]: ids,
                 },
-                
-            },
-            individualHooks: true
-        })
-    }
+        },
+        individualHooks: true,
+        transaction: this.transaction,
+      }
+    );
+  }
 
-    async getSessions(createdAt: Date):Promise<ISession[]>{
-        return await SessionsDb.findAll(
-            {
-                where:{
-                    createdAt:{
-                        [Op.gte]: createdAt
-                    }
-                }
-            }
-        )
-    }
+  async getSessions(createdAt: Date): Promise<ISession[]> {
+    return await SessionsDb.findAll({
+      where: {
+        createdAt: {
+          [Op.gte]: createdAt,
+        },
+      },
+      transaction: this.transaction,
+    });
+  }
 
-    async getSessionsByInstructorId(instructorId:number,status:ISessionStatus,createdAt:Date):Promise<ISession[]>{
-        return await SessionsDb.findAll({
-            where:{
-                instructorId: Number(instructorId),
-                status,
-                createdAt: {[Op.gte]: createdAt}
-            }
-        })
-    }
+  async getSessionsByInstructorId(
+    instructorId: number,
+    status: ISessionStatus,
+    createdAt: Date
+  ): Promise<ISession[]> {
+    return await SessionsDb.findAll({
+      where: {
+        instructorId: Number(instructorId),
+        status,
+        createdAt: { [Op.gte]: createdAt },
+      },
+      transaction: this.transaction,
+    });
+  }
 
-    
+  async getTransactions() {
+    this.transaction = await sequelize.transaction();
+  }
 }
-
